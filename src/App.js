9db83a1a68cfc80802021065a1f4b9852a7d28f0
Edita Keškić <edita.keskic@gmail.com>
2021-06-19 18:32:44 +0200

    Added Switch component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import { Route } from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
 
-import AllMeetupsPage from  './pages/AllMeetups';
-import NewMeetupPage from './pages/NewMeetup';
-import FavouritesPage from './pages/Favourites';
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavouritesPage from "./pages/Favourites";
 
 function App() {
-  return <div>
-    <Route path='/'>
-      <AllMeetupsPage />
-    </Route>
-    <Route path='/new-meetup'>
-      <NewMeetupPage />
-    </Route>
-    <Route path='Favourites'>
-      <FavouritesPage />
-    </Route>
-  </div>;
+  return (
+    <div>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="Favourites">
+          <FavouritesPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;

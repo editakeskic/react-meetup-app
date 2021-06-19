3553bd4f42ed6931939d03bc001748acd5574e3f
Edita Keškić <edita.keskic@gmail.com>
2021-06-19 22:42:40 +0200

    Created Navigation file

---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,13 @@
+function MainNavigation() {
+  return <header>
+      <div>React Meetups</div>
+      <nav>
+          <ul>
+              <li></li>
+          </ul>
+      </nav>
+
+  </header>
+}
+
+export default MainNavigation;

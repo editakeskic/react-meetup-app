526d6cef6a6c504edcc6561cd858b24de4f67f1d
Edita Keškić <edita.keskic@gmail.com>
2021-06-19 17:37:09 +0200

    Import components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import { Route } from 'react-router-dom';
+
+import AllMeetupsPage from  './pages/AllMeetups ';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoutritesPage from './pages/Favourites';
+
 function App() {
   return <div></div>;
 }

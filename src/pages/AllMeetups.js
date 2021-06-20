120fac4ca41139b89fbc2a448c15454ddc09372e
Edita Keškić <edita.keskic@gmail.com>
2021-06-20 21:42:48 +0200

    Imported MeetupList component

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,3 +1,5 @@
+import MeetupList from '../components/meetups/MeetupList';
+
 const DUMMY_DATA = [
     {
       id: 'm1',
@@ -20,13 +22,9 @@ const DUMMY_DATA = [
   ];
 
 function AllMeetupsPage () {
-    return <section>All Meetups Page
+    return <section>
         <h1>All Meetups</h1>
-        <ul>
-        {DUMMY_DATA.map((meetup) => {
-            return <li key={meetup.id}>{meetup.title}</li>;
-        })}
-        </ul>
+        <MeetupList meetups={DUMMY_DATA} />
     </section>
 }
 

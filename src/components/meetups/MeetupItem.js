86c2c61a9fa23d1d553b1b453f00a4a7104a73d0
Edita Keškić <edita.keskic@gmail.com>
2021-06-20 20:43:29 +0200

    Basic structure of MeetupItem

---
+++ src/components/meetups/MeetupItem.js
@@ -0,0 +1,17 @@
+function MeetupItem() {
+    return <li>
+        <div>
+            <img src="" alt="" />
+        </div> 
+        <div>
+            <h3>TITLE</h3>
+            <address>ADDRESS</address>
+            <p>DESCRIPTION</p>
+        </div>
+        <div>
+            <button>To Favourites</button>
+        </div>
+    </li>
+}
+
+export default MeetupItem;

d47ae6564918feaaa9f33d56e7d131856ff8b559
Edita Keškić <edita.keskic@gmail.com>
2021-06-20 19:22:04 +0200

    Added links to MainNavigation

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,9 +1,20 @@
+import { Link } from 'react-router-dom';
+
 function MainNavigation() {
   return <header>
       <div>React Meetups</div>
       <nav>
           <ul>
-              <li></li>
+              <li>
+                  <Link to='/'>All Meetups</Link>
+              </li>
+              <li>
+                  <Link to='/new-meetup'>Add New Meetup</Link>
+              </li>
+              <li>
+                  <Link to='/favourites'>My Favourites</Link>
+              </li>
+
           </ul>
       </nav>
 

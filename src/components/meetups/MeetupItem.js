d4a80c1fff1dc61fa7e8c4624dfce2c7fad9a8aa
Edita Keškić <edita.keskic@gmail.com>
2021-06-20 21:10:56 +0200

    Added CSS classes to MeetupItem

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,17 +1,21 @@
+import classes from './MeetupItem.module.css'
+
 function MeetupItem(props) {
-    return <li>
-        <div>
-            <img src={props.image} alt={props.title} />
-        </div> 
-        <div>
-            <h3>{props.title}</h3>
-            <address>{props.address}</address>
-            <p>{props.description}</p>
-        </div>
-        <div>
-            <button>To Favourites</button>
-        </div>
+  return (
+    <li className={classes.item}>
+      <div className={classes.image}>
+        <img src={props.image} alt={props.title} />
+      </div>
+      <div className={classes.content}>
+        <h3>{props.title}</h3>
+        <address>{props.address}</address>
+        <p>{props.description}</p>
+      </div>
+      <div className={classes.actions}>
+        <button>To Favourites</button>
+      </div>
     </li>
+  );
 }
 
 export default MeetupItem;

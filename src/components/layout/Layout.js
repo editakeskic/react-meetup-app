76e5c0bf1a17ce1e8910499000eaa3e0e96b41fb
Edita Keškić <edita.keskic@gmail.com>
2021-06-22 17:22:57 +0200

    Added props and CSS class

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,11 +1,11 @@
 import MainNavigation from "./MainNavigation";
 import classes from "./Layout.module.css";
 
-function Layout() {
+function Layout(props) {
   return (
     <div>
       <MainNavigation />
-      <main></main>
+      <main className={classes.main}>{props.children}</main>
     </div>
   );
 }

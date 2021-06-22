8a535ccda572b5106ba985f85bd097342df5df32
Edita Keškić <edita.keskic@gmail.com>
2021-06-22 17:16:55 +0200

    Created Layout component

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,13 @@
+import MainNavigation from "./MainNavigation";
+import classes from "./Layout.module.css";
+
+function Layout() {
+  return (
+    <div>
+      <MainNavigation />
+      <main></main>
+    </div>
+  );
+}
+
+export default Layout;
